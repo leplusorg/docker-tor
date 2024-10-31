4186d8849d0329ec903ff3dcde4de30e0314d87b
Thomas Leplus <thomasleplus@users.noreply.github.com>
2024-10-31 14:24:18 +0545

    Fix error

--- tor/tor-wrapper.sh
+++ tor/tor-wrapper.sh
@@ -9,25 +9,41 @@ fi
 
 # Honoring GitHub runner debug mode
 if [ -n "${RUNNER_DEBUG+x}" ] && [ "${RUNNER_DEBUG}" = 1 ]; then
-	DEBUG=true
+	DEBUG=false
 fi
 
 if [ "${DEBUG}" = true ]; then
 	set -o xtrace
+	LOG_LEVEL=debug
 fi
 
 if [ -f '/etc/torrc' ]; then
 	\echo 'WARN: Ignoring /etc/torrc, new location is /etc/tor/torrc.'
 fi
 
-if [ -f /etc/tor/torrc ]; then
-	\echo 'INFO: Found existing /etc/tor/torrc file, skipping template.'
+
+if [ -z "${SHELL_FORMAT+x}" ]; then
+	# Using single quotes here on purpose, we don't want the
+	# variables names to be expanded.
+	SHELL_FORMAT='${DATA_DIRECTORY},${LOG_LEVEL},${LOG_FILE},${SOCKS_HOSTNAME},${SOCKS_PORT}'
 else
+    \echo "DEBUG: Found custom Shell Format for envsubst: ${SHELL_FORMAT}"
+fi
+
+DATA_DIRECTORY="${DATA_DIRECTORY:-/var/lib/tor}" \
+	LOG_LEVEL="${LOG_LEVEL:-notice}" \
+	LOG_FILE="${LOG_FILE:-stdout}" \
 	SOCKS_HOSTNAME="${SOCKS_HOSTNAME:-127.0.0.1}" \
-		SOCKS_PORT="${SOCKS_PORT:-9050}" \
-		envsubst </etc/tor/torrc.sample >/etc/tor/torrc
+	SOCKS_PORT="${SOCKS_PORT:-9050}" \
+	envsubst "${SHELL_FORMAT}" </etc/tor/torrc.template >/etc/tor/torrc
+
+if [ "${DEBUG}" = true ]; then
+    \echo 'DEBUG: Content of /etc/tor/torrc:'
+    \echo 'DEBUG: =========================='
+    \cat /etc/tor/torrc | \sed -e 's/^/DEBUG: /'
+    \echo 'DEBUG: =========================='
 fi
 
 cmd=$(\which tor)
 
-"${cmd}" "$@"
+"${cmd}" -f /etc/tor/torrc "$@"
