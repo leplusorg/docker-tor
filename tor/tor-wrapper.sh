c7ef4935ec787daecfe5bfe14bd4bcc9bf9e2404
Thomas Leplus <thomasleplus@users.noreply.github.com>
2024-10-31 14:33:08 +0545

    Update tor-wrapper.sh

--- tor/tor-wrapper.sh
+++ tor/tor-wrapper.sh
@@ -18,30 +18,31 @@ if [ "${DEBUG}" = true ]; then
 fi
 
 if [ -f '/etc/torrc' ]; then
-	\echo 'WARN: Ignoring /etc/torrc, new location is /etc/tor/torrc.'
-fi
-
-
-if [ -z "${SHELL_FORMAT+x}" ]; then
-	# Using single quotes here on purpose, we don't want the
-	# variables names to be expanded.
-	SHELL_FORMAT='${DATA_DIRECTORY},${LOG_LEVEL},${LOG_FILE},${SOCKS_HOSTNAME},${SOCKS_PORT}'
+	if [ "${DEBUG}" = true ]; then
+		\echo 'DEBUG: Found existing /etc/torrc, overwritting /etc/tor/torrc.'
+	fi
+	\cp -f '/etc/torrc' '/etc/tor/torrc'
 else
-    \echo "DEBUG: Found custom Shell Format for envsubst: ${SHELL_FORMAT}"
+	if [ -z "${SHELL_FORMAT+x}" ]; then
+		# Using single quotes here on purpose, we don't want the
+		# variables names to be expanded.
+		SHELL_FORMAT='${DATA_DIRECTORY},${LOG_LEVEL},${LOG_FILE},${SOCKS_HOSTNAME},${SOCKS_PORT}'
+	elif [ "${DEBUG}" = true ]; then
+		\echo "DEBUG: Found custom Shell Format for envsubst: ${SHELL_FORMAT}"
+	fi
+	DATA_DIRECTORY="${DATA_DIRECTORY:-/var/lib/tor}" \
+		LOG_LEVEL="${LOG_LEVEL:-notice}" \
+		LOG_FILE="${LOG_FILE:-stdout}" \
+		SOCKS_HOSTNAME="${SOCKS_HOSTNAME:-127.0.0.1}" \
+		SOCKS_PORT="${SOCKS_PORT:-9050}" \
+		envsubst "${SHELL_FORMAT}" </etc/tor/torrc.template >/etc/tor/torrc
 fi
-
-DATA_DIRECTORY="${DATA_DIRECTORY:-/var/lib/tor}" \
-	LOG_LEVEL="${LOG_LEVEL:-notice}" \
-	LOG_FILE="${LOG_FILE:-stdout}" \
-	SOCKS_HOSTNAME="${SOCKS_HOSTNAME:-127.0.0.1}" \
-	SOCKS_PORT="${SOCKS_PORT:-9050}" \
-	envsubst "${SHELL_FORMAT}" </etc/tor/torrc.template >/etc/tor/torrc
 
 if [ "${DEBUG}" = true ]; then
-    \echo 'DEBUG: Content of /etc/tor/torrc:'
-    \echo 'DEBUG: =========================='
-    \cat /etc/tor/torrc | \sed -e 's/^/DEBUG: /'
-    \echo 'DEBUG: =========================='
+	\echo 'DEBUG: Content of /etc/tor/torrc:'
+	\echo 'DEBUG: =========================='
+	\cat /etc/tor/torrc | \sed -e 's/^/DEBUG: /'
+	\echo 'DEBUG: =========================='
 fi
 
 cmd=$(\which tor)
